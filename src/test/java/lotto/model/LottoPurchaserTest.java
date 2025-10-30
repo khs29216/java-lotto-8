ce7b1aaa8beb4fc6309e8462e140fbfbb1d7374c
khs29216 <khs29216@gmail.com>
2025-10-31 01:59:27 +0900

    test(LottoPurchaser): 로또 구입 금액 객체 생성 테스트 추가
    - 정상 입력일 때 올바르게 LottoPurchaser 객체가 생성되는지 테스트 추가
    - 유효하지 않은 입력값일 때 예외 테스트 추가
        - 구매금액이 0이하의 숫자일 경우
        - 구매금액이 1000원으로 나누어 떨어지지 않는 경우

---
+++ src/test/java/lotto/model/LottoPurchaserTest.java
@@ -0,0 +1,39 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoPurchaserTest {
+    @Test
+    public void 정상적인_입력값일_경우_구매_금액으로_LottoPurchaser_객체를_생성한다() {
+        // given
+        int purchaseAmount = 14000;
+        LottoPurchaser expected = new LottoPurchaser(14000);
+
+        // when
+        LottoPurchaser actual = new LottoPurchaser(purchaseAmount);
+
+        // then
+        assertThat(actual.getPurchaseAmount())
+                .isEqualTo(expected.getPurchaseAmount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    public void 입력값이_0이하의_숫자일_경우_예외가_발생한다(int purchaseAmount) {
+        assertThatThrownBy(() -> new LottoPurchaser(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {14001, 12345})
+    public void 입력값이_1000원으로_나누어_떨어지지_않으면_예외가_발생한다(int purchaseAmount) {
+        assertThatThrownBy(() -> new LottoPurchaser(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
